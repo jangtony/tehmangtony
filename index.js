27923f2a3a65042a167e98933f98345fe0cd6544
jangtony <163066529+jangtony@users.noreply.github.com>
2024-03-12 19:11:44 +0700

    index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+const count = require('text-count');
+
+function tehmangtony) {
+  const text = "T E H M A N G T O N Y"
+  return {
+    text,
+    length: count.charCount(text)
+  }
+}
+
+
+module.exports = Tehmangtony
